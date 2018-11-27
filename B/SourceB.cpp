08f5f3a7fecee3a05243653fb5787b60a2aa6f4a
ArtVad3r <39739339+ArtVad3r@users.noreply.github.com>
2018-11-27 23:20:55 +0100

    Update SourceB.cpp

--- B/SourceB.cpp
+++ B/SourceB.cpp
@@ -79,3 +79,79 @@ int main()
 
 	return 0;
 }
+
+
+________________________________________________________
+
+
+
+
+#include "cccaus.h"
+
+int main(){
+    char msto_autobaz[30], znck_typ_auta[30];
+    int Rok, Cislo, Cena;
+    int Pocet;
+    int i = 0, j = 0;
+    double sum = 0;
+
+    cccaus objekty[5];
+    cout << "Vlozte pocet (max. 3):  ";
+    cin >> Pocet;
+    if ((Pocet > 3) || (Pocet == 0))
+          cout << "Zly pocet" << endl;
+
+    else{
+        while (i < Pocet){
+           i++;
+           cout << endl << "vlozte Miesto " << i << ". auta      : ";
+           cin >> msto_autobaz;
+           cout << "vlozte Znacku " << i << ". auta : ";
+           cin >> znck_typ_auta;
+           cout << "vlozte Rok " << i << ". auta : ";
+           cin >> Rok;
+           cout << "vlozte Cislo " << i << ". auta : ";
+           cin >> Cislo;
+           cout << "vlozte Cenu " << i << ". auta : ";
+           cin >> Cena;
+
+           objekty[i].SetPlc(msto_autobaz);
+           objekty[i].SetSgn(znck_typ_auta);
+           objekty[i].SetYr(Rok);
+           objekty[i].SetNmb(Cislo);
+           objekty[i].SetPrce(Cena);
+
+        }
+
+       i++;
+       objekty[i].SetPlc((char*)"Trnava");
+       objekty[i].SetSgn((char*)"Nissan_Primera");
+       objekty[i].SetYr(2006);
+       objekty[i].SetNmb(10020);
+       objekty[i].SetPrce(3300);
+
+       sum += objekty[i].GetPrce();
+       cout << endl << endl;
+
+       cout << "programom vytvoreny a inicializovany objekt auta pomocou parametrickeho konstruktora triedy 'cccaus': " << endl;
+       cout << "Miesto autobazara s pred. autom: " << objekty[i].GetPlc() << endl;
+       cout << "Znacka a typ auta : " << objekty[i].GetSgn() << endl;
+       cout << "Rok vyroby auta    : " << objekty[i].GetYr() << endl;
+       cout << "predajne cislo auta  : " << objekty[i].GetNmb() << endl;
+       cout << "predajna cena auta [Eur]   : " << objekty[i].GetPrce() << endl;
+       cout << endl << "_______________________________________________________________" << endl;
+       cout << "hodnoty instan. premennych objektov triedy 'cccaus' (aut), vlozene pouzivatelom programu: " << endl;
+        for (i = 1; i < Pocet + 1; i++){
+           cout << "Miesto autobazara s pred. autom: " << objekty[i].GetPlc() << endl;
+           cout << "Znacka a typ auta: " << objekty[i].GetSgn() << endl;
+           cout << "Rok vyroby auta   : " << objekty[i].GetYr() << endl;
+           cout << "predajne cislo auta  : " << objekty[i].GetNmb() << endl;
+           cout << "predajna cena auta [Eur]   : " << objekty[i].GetPrce() << endl;
+           cout << endl << "_______________________________________________________________" << endl;
+
+           sum += objekty[i].GetPrce();
+        }
+    }
+    cout << "Priemerna predajna cena uvedenych 4-roch aut je: " << (sum / (Pocet + 1)) << endl;
+    return 0;
+}
