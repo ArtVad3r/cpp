751f69bff44dd16f5369a0dea5e8287fd637b31d
ArtVad3r <39739339+ArtVad3r@users.noreply.github.com>
2018-11-27 23:21:21 +0100

    Update Automobil.h

--- B/Automobil.h
+++ B/Automobil.h
@@ -26,3 +26,36 @@ public:
 	void SetCislo(int Number);
 	void SetCena(long int  Price);
 };
+
+
+__________________________________
+
+
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class cccaus{
+private:
+    char msto_autobaz[30];
+    char znck_typ_auta[30];
+    int rok_vrby_auta;
+    int prdj_cis_auta;
+    long int cna_auta;
+public:
+    cccaus() {}
+    cccaus(char *miesto, char *znacka, int rok, int cislo, long int  cena);
+
+    char *GetPlc();
+    char *GetSgn();
+    int GetYr();
+    int GetNmb();
+    long int GetPrce();
+
+    void SetPlc(char *Place);
+    void SetSgn(char *Brand);
+    void SetYr(int Year);
+    void SetNmb(int Number);
+    void SetPrce(long int  Price);
+};
