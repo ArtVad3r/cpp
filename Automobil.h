6cdcd38821acc295ef92de71d36167d365d94fad
ArtVad3r <39739339+ArtVad3r@users.noreply.github.com>
2018-11-27 20:21:20 +0100

    Create Automobil.h

---
+++ Automobil.h
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Automobil
+{
+private:
+	char miesto_autobaz[30];
+	char znacka_typ_auta[30];
+	int rok_vyroby_auta;
+	int predaj_cis_auta;
+	long int cena_auta;
+public:
+	Automobil() {}
+	Automobil(char *miesto, char *znacka, int rok, int cislo, long int  cena);
+
+	char *GetMiesto(); //ziskame hodnoty parametrov
+	char *GetZnacka();
+	int GetRok();
+	int GetCislo();
+	long int  GetCena();
+
+	void SetMiesto(char *Place);
+	void SetZnacka(char *Brand);
+	void SetRok(int Year);
+	void SetCislo(int Number);
+	void SetCena(long int  Price);
+};
