e014a55ef470b43e5e665068b60eef5ad85bc6ab
ArtVad3r <39739339+ArtVad3r@users.noreply.github.com>
2018-11-27 23:21:10 +0100

    Update Automobil.cpp

--- B/Automobil.cpp
+++ B/Automobil.cpp
@@ -53,3 +53,60 @@ long int  Automobil::GetCena()
 {
 	return cena_auta;
 }
+
+
+
+___________________________________
+
+
+
+
+
+#include "cccaus.h"
+
+cccaus::cccaus(char *miesto, char *znacka, int rok, int cislo, long int  cena){
+	strcpy(cccaus::msto_autobaz, miesto);
+	strcpy(cccaus::znck_typ_auta, znacka);
+    cccaus::rok_vrby_auta = rok;
+    cccaus::prdj_cis_auta = cislo;
+    cccaus::cna_auta = cena;
+}
+
+
+
+void cccaus::SetPlc(char *Place){
+	strcpy(msto_autobaz, Place);
+}
+char *cccaus::GetPlc(){
+	return msto_autobaz;
+}
+
+
+
+void cccaus::SetSgn(char *Brand){
+	strcpy(znck_typ_auta, Brand);
+}
+char *cccaus::GetSgn(){
+	return znck_typ_auta;
+}
+
+void cccaus::SetYr(int Year){
+    rok_vrby_auta = Year;
+}
+int cccaus::GetYr(){
+	return rok_vrby_auta;
+}
+
+void cccaus::SetNmb(int Number){
+    prdj_cis_auta = Number;
+}
+int cccaus::GetNmb(){
+	return prdj_cis_auta;
+}
+
+void cccaus::SetPrce(long int Price){
+    cna_auta = Price;
+}
+long int  cccaus::GetPrce(){
+	return cna_auta;
+}
